ccb86d44ec5484a16c98f9ce0ce4d45ba0221f4e
Ankit Yadav <ankitya797@gmail.com>
2026-03-13 10:20:56 +0530

    Update Matrix_printing.py

--- Python_code/Matrix_printing.py
+++ Python_code/Matrix_printing.py
@@ -7,13 +7,47 @@ print("Enter the entries rowwise: ")
 
 #For user input
 for i in range(R):
-  a = []
-  for j in range(C):
-    a.append(int(input()))
-  matrix.append(a)
+    a = []
+    for j in range(C):
+        a.append(int(input()))
+    matrix.append(a)
 
 # For i in range(R):
-for j in range(R):
+for i in range(R):
   for j in range(C):
-      print(matrix[i][j]),end=" ")
+    print(matrix[i][j],end=" ")
   print()
+
+#Addition of matrix elements:
+
+total = 0
+
+for i in range(R):
+   for j in range(C):
+      total = total + matrix[i][j]
+
+print(total)
+
+# Addition of matrices:
+
+matrix2 = []
+
+
+# Taking input:
+
+
+for i in range(R):
+  b = []
+  for j in range(C):
+    b.append(int(input()))
+  matrix2.append(b)
+
+TotalMax = 0
+
+for i in range(R):
+  for j in range(C):
+     TotalMax = TotalMax + matrix[i][j] + matrix2[i][j]
+
+print(TotalMax)
+
+   
